e37c6b031e1d88100d9fa9803a5233fe6fdea617
Thom van Kalkeren <thomvankalkeren@gmail.com>
2023-05-08 10:27:56 +0200

    feat: add topic play/pause function

--- frontend/src/modules/Topics/TopicOutput.svelte
+++ frontend/src/modules/Topics/TopicOutput.svelte
@@ -1,51 +1,69 @@
 <script lang="ts">
-    import type { Topic } from "$lib/topics";
-    import { onDestroy, onMount } from "svelte";
-    import { register, sendCommand } from "$lib/socket";
-    import { Button, Chevron, Dropdown, DropdownItem } from "flowbite-svelte";
+  import { Button, Chevron, Dropdown, DropdownItem, P } from "flowbite-svelte";
 
-    export let topic: Topic;
+  import type { Topic } from "$lib/topics";
+  import { onDestroy, onMount } from "svelte";
+  import { register, sendCommand } from "$lib/socket";
 
-    let messages = new Array(50);
+  import PlayIcon from "../../icons/PlayIcon.svelte";
+  import PauseIcon from "../../icons/PauseIcon.svelte";
 
-    const subscription = register("Message.TopicMessage");
+  const start = () => {
+    sendCommand({ type: "Command.SubscribeTopic", id: topic.id });
+  };
+  const stop = () => {
+    sendCommand({ type: "Command.UnsubscribeTopic", id: topic.id });
+  };
 
-    $: {
-      if ($subscription) {
-        messages.pop()
-        messages.unshift($subscription.message);
-        messages = messages;
-      }
+  export let topic: Topic;
+
+  let messages = new Array(50);
+  let paused = false;
+
+  const subscription = register("Message.TopicMessage");
+
+  $: paused ? stop() : start();
+
+  $: {
+    if ($subscription) {
+      messages.pop();
+      messages.unshift($subscription.message);
+      messages = messages;
     }
+  }
 
-    const start = () => {
-      sendCommand({ type: 'Command.SubscribeTopic', id: topic.id });
-    };
-    const stop = () => {
-      sendCommand({ type: 'Command.UnsubscribeTopic', id: topic.id });
-    };
+  const resize = (size: number) => () => {
+    const copy = new Array(size);
+    copy.unshift(...messages.slice(0, Math.min(size, messages.length)));
+    messages = copy;
+  };
 
-    const resize = (size: number) => () => {
-      const copy = new Array(size);
-      copy.unshift(...messages.slice(0, Math.min(size, messages.length)))
-      messages = copy;
-    }
-
-    onMount(start);
-    onDestroy(stop);
+  onMount(start);
+  onDestroy(stop);
 </script>
 
 <div class="flex flex-col w-full">
     <div class="flex flex-row justify-between">
         <h3 class="text-2xl">{topic?.id}</h3>
-        <Button><Chevron>{messages.length} Items</Chevron></Button>
-        <Dropdown>
-            <DropdownItem on:click={resize(10)}>10</DropdownItem>
-            <DropdownItem on:click={resize(30)}>30</DropdownItem>
-            <DropdownItem on:click={resize(50)}>50</DropdownItem>
-            <DropdownItem on:click={resize(100)}>100</DropdownItem>
-            <DropdownItem on:click={resize(1000)}>1000</DropdownItem>
-        </Dropdown>
+        <div class="flex gap-1">
+            <Button>
+                <Chevron>{messages.length} Items</Chevron>
+            </Button>
+            <Dropdown>
+                <DropdownItem on:click={resize(10)}>10</DropdownItem>
+                <DropdownItem on:click={resize(30)}>30</DropdownItem>
+                <DropdownItem on:click={resize(50)}>50</DropdownItem>
+                <DropdownItem on:click={resize(100)}>100</DropdownItem>
+                <DropdownItem on:click={resize(1000)}>1000</DropdownItem>
+            </Dropdown>
+            <Button on:click={() => paused = !paused}>
+                {#if paused}
+                    <PlayIcon />
+                {:else}
+                    <PauseIcon />
+                {/if}
+            </Button>
+        </div>
     </div>
     <div>
         {#each messages as message}
