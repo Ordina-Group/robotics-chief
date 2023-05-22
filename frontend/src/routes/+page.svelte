e879fcd3d982f543ac66aed3f9377b400032a112
Thom van Kalkeren <thomvankalkeren@gmail.com>
2023-05-22 15:58:54 +0200

    feat: livestream page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -9,6 +9,8 @@
   import StatusBar from "../modules/StatusBar/StatusBar.svelte";
   import SettingsIcon from "../icons/SettingsIcon.svelte";
   import Settings from "../modules/Settings/Settings.svelte";
+  import VideoIcon from "../icons/VideoIcon.svelte";
+  import Livestream from "../modules/Livestream/Livestream.svelte";
 </script>
 
 <h1 class="text-6xl">Welcome to RoboChief</h1>
@@ -33,6 +35,16 @@
         </div>
         <div class="grid gap-2">
             <Topics />
+        </div>
+    </TabItem>
+
+    <TabItem>
+        <div slot="title" class="flex items-center gap-2">
+            <VideoIcon class="h-5 w-auto" />
+            Livestream
+        </div>
+        <div class="grid gap-2">
+            <Livestream />
         </div>
     </TabItem>
 
