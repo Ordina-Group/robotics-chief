6c23d7c24dd39fb01cb02bba57ececdd52d52b24
Thom van Kalkeren <thomvankalkeren@gmail.com>
2023-05-22 15:58:54 +0200

    feat: livestream page

---
+++ frontend/src/modules/Livestream/Livestream.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { settingsStore } from "$lib/dashboard";
+</script>
+
+<img
+    id="livestream"
+    src="http://{$settingsStore.host}:8080/livestream"
+    alt="video"
+    height="720"
+    width="480"
+/>
